ed4192d403a4f8503660471f8c97a5437d867e3a
minhln125 <minhln125@gmail.com>
2022-10-18 17:08:39 +0700

    add option chrome

--- features/step_definitions/step.rb
+++ features/step_definitions/step.rb
@@ -1,8 +1,9 @@
 require 'selenium-cucumber'
 require 'appium_lib'
 options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--no-sandbox')
 options.add_argument('--headless')
-
+options.add_argument('--disable-gpu')
 caps = Selenium::WebDriver::Remote::Capabilities.chrome("goog:chromeOptions" => {detach: true})
 driver = Selenium::WebDriver.for :chrome ,capabilities: caps, options: options
 Given('on page login') do
