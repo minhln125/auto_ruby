70e0996a65e39e82953682bef952bb86681e423f
minhln125 <minhln125@gmail.com>
2022-10-25 15:03:44 +0700

    change saucelab

--- features/step_definitions/step.rb
+++ features/step_definitions/step.rb
@@ -4,8 +4,8 @@ options = Selenium::WebDriver::Options.firefox
 options.browser_version = 'latest'
 options.platform_name = 'Windows 10'
 sauce_options = {}
-sauce_options[:build] = '<your build id>'
-sauce_options[:name] = '<your test name>'
+sauce_options[:build] = '<N01>'
+sauce_options[:name] = '<Testsaucelab>'
 options.add_option('sauce:options', sauce_options)
 
 url = 'https://oauth-minhln125-44ef9:26839fe5-aa95-4ac3-8904-b6b72f550922@ondemand.apac-southeast-1.saucelabs.com:443/wd/hub'
